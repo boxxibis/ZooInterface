f0921e6ff6aa2cc8b1cb0458c873c6dc04bf16d8
cspencer8684 <cspencer8684@001LAB102010540.neisd.ad>
2015-03-20 11:48:46 -0500

    Cat ready for animation

--- asdf/src/Cat.java
+++ asdf/src/Cat.java
@@ -14,52 +14,60 @@ class DrawPanel extends JPanel {
 	private void doDrawing(Graphics g) {
 
 		Graphics2D g2d = (Graphics2D) g;
+		//Variables
+		int d = 700; //X-Value
+		int f = 100; //Y-Value
+		int h = 100; //X-Size
+		int j = 100; //Y-Size
 		
+		
+		//Background
 		g2d.setColor(new Color(0, 51, 102));
 		g2d.fillRect(0,0,2000,2000);
+		//Stars
+		Random rndmInt = new Random();
 		
+		for (int b = 1; b <=25; b++)
+		{
+			int q = rndmInt.nextInt(1200);
+			int w = rndmInt.nextInt(1200);
+			int red = (255);
+			int green = (255);
+			int blue =(255);
+			g.setColor(new Color(red,green,blue));
+			g.fillRect(q, w, 10, 10);
+		}
 		//Poptart
 				g2d.setColor(new Color(254,202,162));
-				g2d.fillRect(700, 175, 475, 375);
+				g2d.fillRect(d, f + 75, h + 375, j + 275);
 				
-		Random rndmInt = new Random();
-				
-				for (int b = 1; b <=15; b++)
-				{
-					int q = rndmInt.nextInt(1200);
-					int w = rndmInt.nextInt(1200);
-					int red = (255);
-					int green = (255);
-					int blue =(255);
-					g.setColor(new Color(red,green,blue));
-					g.fillRect(q, w, 10, 10);
-				}
+		
 		//Poptart Black Outline
 				
 				g2d.setColor(Color.black);
 				
-				g2d.fillRect(700,175,25,25);
-				g2d.fillRect(700,525, 25, 25);
-				g2d.fillRect(675,200, 25, 350);
-				g2d.fillRect(725, 150, 425, 25);
-				g2d.fillRect(1150, 175, 25, 25);
-				g2d.fillRect(1175, 200, 25, 150);
-				g2d.fillRect(725, 550, 300,25);
+				g2d.fillRect(d, f + 75, h - 75, j - 75);
+				g2d.fillRect(d, f + 425, h - 75, j - 75);
+				g2d.fillRect(d - 25, f + 100, h - 75, j + 250);
+				g2d.fillRect(d + 25, f + 50, h + 325, j - 75);
+				g2d.fillRect(d + 450, f + 75, h - 75, j - 75);
+				g2d.fillRect(d + 475, f + 100, h - 75, j + 50);
+				g2d.fillRect(d + 25, f + 450, h + 200, j - 75);
 				
 		//Poptart Topping
 				g2d.setColor(new Color(255,153,255));
 				
-				g2d.fillRect(725, 200, 425, 325);
+				g2d.fillRect(d + 25, f + 100, h + 325, j + 225);
 				
 		//Poptart (Continued)
 				g2d.setColor(new Color(254,202,162));
 				
-				g2d.fillRect(725, 200, 25, 50);
-				g2d.fillRect(725, 200, 50, 25);
-				g2d.fillRect(1100, 200, 50, 25);
-				g2d.fillRect(1125, 200, 25, 50);
-				g2d.fillRect(725, 475, 25, 50);
-				g2d.fillRect(725, 500, 50, 25);
+				g2d.fillRect(d + 25, f + 200, h - 75, j - 50);
+				g2d.fillRect(d + 25, f + 100, h - 50, j - 75);
+				g2d.fillRect(d + 400, f + 100, h - 50, j - 75);
+				g2d.fillRect(d + 425, f + 100, h - 75, j - 50);
+				g2d.fillRect(d + 25, f + 375, h - 75, j - 50);
+				g2d.fillRect(d - 25, f - 400, h - 50, j - 75);
 				
 		//Poptart Sprinkles
 				Random rndInt = new Random();
@@ -78,63 +86,63 @@ class DrawPanel extends JPanel {
 		//Head Black Outline
 				g2d.setColor(Color.black);
 				
-				g2d.fillRect(1000, 275, 50, 275);
-				g2d.fillRect(975, 300, 25, 225);
-				g2d.fillRect(950, 400, 25, 100); //Here
-				g2d.fillRect(1050, 300, 25, 25);
-				g2d.fillRect(1075, 325, 25, 25);
-				g2d.fillRect(1050, 350, 50, 175);
-				g2d.fillRect(1100, 350, 100, 200);
-				g2d.fillRect(1025, 525, 250, 50);
-				g2d.fillRect(1200, 325, 50, 175);
-				g2d.fillRect(1225, 300, 25, 25);
-				g2d.fillRect(1250, 275, 50, 225);
-				g2d.fillRect(1300, 400, 50, 100);
-				g2d.fillRect(1300, 300, 25, 100);
-				g2d.fillRect(1300, 500, 25, 25);
-				g2d.fillRect(1275, 525, 25, 25);
+				g2d.fillRect(d + 300, f + 175, h - 50, j + 175);
+				g2d.fillRect(d + 275, f + 200, h - 75, j + 125);
+				g2d.fillRect(d + 250, f + 300, h - 75, j); 
+				g2d.fillRect(d + 350, f + 200, h - 75, j - 75);
+				g2d.fillRect(d + 375, f + 225, h - 75, j - 75);
+				g2d.fillRect(d + 350, f + 250, h - 50, j + 75);
+				g2d.fillRect(d + 400, f + 250, h, j + 100);
+				g2d.fillRect(d + 325, f + 425, h + 150, j - 50);
+				g2d.fillRect(d + 500, f + 225, h - 50, j + 75);
+				g2d.fillRect(d + 525, f + 200, h - 75, j - 75);
+				g2d.fillRect(d + 550, f + 175, h - 50, j + 125);
+				g2d.fillRect(d + 600, f + 300, h - 50, j);
+				g2d.fillRect(d + 600, f + 200, h - 75, j);
+				g2d.fillRect(d + 600, f + 400, h - 75, j - 75);
+				g2d.fillRect(d + 575, f + 425, h - 75, j - 75);
 		//Head
 				g2d.setColor(Color.gray);
 				
 				//g2d.drawRect(1000,300,300,300);
-				g2d.fillRect(1000,300,50,225);
-				g2d.fillRect(975,400,25,100);
-				g2d.fillRect(1050,325, 25, 25);
-				g2d.fillRect(1050, 350, 50, 175);
-				g2d.fillRect(1100, 375, 100, 150);
-				g2d.fillRect(1025, 525, 250, 25);
-				g2d.fillRect(1200, 350, 50, 175);
-				g2d.fillRect(1225, 325, 25, 25);
-				g2d.fillRect(1250, 300, 50, 225);
-				g2d.fillRect(1300, 400, 25, 100);
+				g2d.fillRect(d + 300, f + 200, h - 50, j + 125);
+				g2d.fillRect(d + 275, f + 300, h - 75, j);
+				g2d.fillRect(d + 350, f + 225, h - 75, j - 75);
+				g2d.fillRect(d + 350, f + 250, h - 50, j + 75);
+				g2d.fillRect(d + 400, f + 275, h, 	   j + 50);
+				g2d.fillRect(d + 325, f + 425, h + 150, j - 75);
+				g2d.fillRect(d + 500, f + 250, h - 50, j + 75);
+				g2d.fillRect(d + 525, f + 225, h - 75, j - 75);
+				g2d.fillRect(d + 550, f + 200, h - 50, j + 125);
+				g2d.fillRect(d + 600, f + 300, h - 75, j);
 				
 		//Cheek Blush
 				g2d.setColor(Color.pink);
 				
-				g2d.fillRect(1000, 450, 50,50);
-				g2d.fillRect(1275, 450, 50,50);
+				g2d.fillRect(d + 300, f + 350, h - 50, j - 50);
+				g2d.fillRect(d + 575, f + 350, h - 50, j - 50);
 				
 		//Eyes
 				g2d.setColor(Color.black);
 				
-				g2d.fillRect(1050, 400, 50, 50);
-				g2d.fillRect(1225, 400, 50,50);
+				g2d.fillRect(d + 350, f + 300, h - 50, j - 50);
+				g2d.fillRect(d + 525, f + 300, h - 50, j - 50);
 				
 				g2d.setColor(Color.white);
 				
-				g2d.fillRect(1050, 400, 25,25);  /*Inner white*/ 
-				g2d.fillRect(1225, 400, 25, 25);  /*Inner white*/ 
+				g2d.fillRect(d + 350, f + 300, h - 75, j - 75);  /*Inner white*/ 
+				g2d.fillRect(d + 525, f + 300, h - 75, j - 75);  /*Inner white*/ 
 				
 		//Nose
 				g2d.setColor(Color.black);
 				
-				g2d.fillRect(1175, 425, 25, 25);
+				g2d.fillRect(d + 475, f + 325, h - 75, j - 75);
 				
 		//Mouth
-				g2d.fillRect(1075, 475, 25, 50);
-				g2d.fillRect(1075, 500, 175, 25);
-				g2d.fillRect(1150, 475, 25, 50);
-				g2d.fillRect(1225, 475, 25, 50);
+				g2d.fillRect(d + 375, f + 375, h - 75, j - 50);
+				g2d.fillRect(d + 375, f + 400, h + 75, j - 75);
+				g2d.fillRect(d + 350, f + 375, h - 75, j - 50);
+				g2d.fillRect(d + 525, f + 375, h - 75, j - 50);
 		
 				
 						
@@ -160,7 +168,7 @@ public class Cat extends JFrame {
 		DrawPanel dpnl = new DrawPanel();
 		add(dpnl);
 
-		setSize(1600, 900);
+		setSize(800,800);
 		setTitle("Nyan_Cat");
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
